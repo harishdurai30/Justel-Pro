4703cd8de61ad7f4e3523e57a3739ff5c850c325
harishdurai30 <harishdurai30@gmail.com>
2026-06-30 09:52:24 +0530

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'counsel@justelpro.com'; 
-        $mail->Password = '@&Un0R57Gy!6'; // ← Must use App Password
+        $mail->Password = ''; // Password
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
